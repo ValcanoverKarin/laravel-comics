0069c4b0f1411133eb6d60f645a79e35911a48b2
karin valcanover <valcanoverkarin@outlook.it>
2021-06-09 01:32:06 +0200

    fine header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,56 @@
 <header>
-    ciao sono l header
+    <!--header top-->
+    <div class="header-top">
+        <div class="container">
+            <ul class="nav-header-top">
+                <li>
+                    <a href="#">DC Power™ Visa®</a>
+                </li>
+                <li>
+                    <a href="#">Additional DC Sites &#9660</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <!--Header bottom -->
+    <div class="header-bottom">
+        <div class="container">
+            <!-- Start Logo -->
+            <div class="logo">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="DC Logo">
+            </div>
+            <!-- End Logo -->
+
+            <!-- Start Nav -->
+            <nav>
+                <ul class="menu">
+                    <li><a href="#">Characters</a></li>
+                    <li class="active"><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Tv</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Collectibles</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">Fans</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Shop <span>&#9660</span></a></li>
+                </ul>
+            </nav>
+
+            
+                <div class="search"> 
+                    <input type="text" placeholder="Search"> 
+                    <i class="fas fa-search"></i>
+                </div>
+           
+            <!-- End Nav -->
+
+        </div>
+    </div>
+    <!-- End Header Bottom -->
+
+    <!-- Start Jumbotron -->
+    <div class="jumbotron"></div>
+    <!-- End Jumbotron -->
 </header>
