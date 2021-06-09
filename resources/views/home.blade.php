67fc0a5cb4f736c67ec151b4fbd685d769d1299e
karin valcanover <valcanoverkarin@outlook.it>
2021-06-09 02:15:11 +0200

    inizio main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.app') 
 
 @section('main_content')
-    <h1>sono la hompage</h1>
+    <!--sezione current series-->
+    <section class="current-series">
+        <div class="container">
+            <h2>Current series</h2>
+
+            <div class="comics-container">
+                   
+                @foreach($comics_array as $comic)
+                    
+                    <!--single comic-->
+                    <div class="comic">
+                        <!--img comic-->
+                        <div class="comic-img">
+                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+                        </div>
+                        <!--end img comic-->
+
+                        <!--titolo comic-->
+                        <div class="comic-title">
+                            <a href="#">{{ $comic['series'] }}</a>
+                        </div>
+                        <!--end titolo comic-->
+                    </div> 
+                    <!--end single comic-->
+
+                @endforeach                            
+
+            </div>
+        </div>
+    </section>
+    <!--end sezione current series-->
 @endsection
